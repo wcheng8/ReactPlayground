8d1114fdd0e7ca3e9a85c98a973fdd80a500f564
Wilkins Cheng <wilkinscheng@MacBook-Pro.local>
2020-06-29 09:49:08 +1000

    Higher Order Components

---
+++ src_HOC/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import ClickCounter from "./ClickCounter";
+import HoverCounter from "./HoverCounter";
+
+function App() {
+	return (
+		<div className="App">
+			<ClickCounter />
+			<HoverCounter />
+		</div>
+	);
+}
+
+export default App;
